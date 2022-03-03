3eaf673d3b00e751328f63d333fcf24360237e87
MATHEMATICS TERRAINS <amankumarpandey2397@gmail.com>
2022-03-04 03:02:43 +0530

    gulp automation for rendering page

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require("gulp"),
+    minimist = require('minimist'),
+    gulpCopy = require('gulp-copy'),
+    replace = require('gulp-string-replace'),
+    rename = require("gulp-rename"),
     autoPrefixer = require("gulp-autoprefixer"),
     argv = require('minimist')(process.argv.slice(2)),
     browserSync = require("browser-sync").create(),
@@ -7,7 +11,7 @@ var gulp = require("gulp"),
     cleanCSS = require("gulp-clean-css"),
     csso = require('gulp-csso'),
     del = require("del");
-    gulpif = require('gulp-if'),
+gulpif = require('gulp-if'),
     sourcemaps = require('gulp-sourcemaps'),
     concat = require('gulp-concat'),
     imagemin = require('gulp-imagemin'),
@@ -18,12 +22,22 @@ var gulp = require("gulp"),
     notify = require('gulp-notify'),
     plumber = require('gulp-plumber'),
     purgecss = require('gulp-purgecss'),
-    nunjucks = require( 'gulp-nunjucks' ) ,
+    nunjucks = require('gulp-nunjucks'),
     rendeNun = require('gulp-nunjucks-render'),
     data = require('gulp-data'),
     lineec = require('gulp-line-ending-corrector'),
     purgecss = require('gulp-purgecss'),
     filter = require('gulp-filter');
+
+
+    var argv = process.argv.slice(1);
+    console.log(argv);
+    const pageName = argv[2];
+    var inputPageLayoutThemeStr = argv[4].replace(/'/g,'"');
+    const inputPageLayoutTheme = JSON.parse(inputPageLayoutThemeStr);
+    console.log(inputPageLayoutTheme);
+    console.log(inputPageLayoutTheme.sectionDetails.length);
+
 
 
 const destination = (argv.clean) ? 'dist/demo/' : (argv.pub) ? 'dist/publish/' : 'dist/';
@@ -33,25 +47,26 @@ var sourcemap = (argv.demo) ? false : (argv.pub) ? true : true;
 var minImg = (argv.demo) ? false : (argv.pub) ? true : false;
 // All Path
 const path = {
-    root:'./',
-    temp:'./app/temp/',
+    root: './',
+    temp: './app/temp/',
     html: './app/*.+(html|njk)',
+    mainPage: '/app/page-template',
     _partialFiles: './app/partials/**/*.+(htm|njk)',
     _partial: './app/partials/',
     fonts: './app/assets/fonts/**/*.*',
     js: './app/assets/js/*.*',
     scss: './app/assets/scss/**/*.scss',
     img: './app/assets/images/**/*.+(png|jpg|gif|ico|svg|webp)',
-    data:'./app/data/data.json',
+    data: './app/data/data.json',
     plugins: './app/plugins/**/*.*',
     pluginCss: './app/plugins/**/*.css',
     bootstrap: {
-        css:'./app/plugins/bootstrap/css/bootstrap.min.css',
-        js:'./app/plugins/bootstrap/js/bootstrap.bundle.min.js'
+        css: './app/plugins/bootstrap/css/bootstrap.min.css',
+        js: './app/plugins/bootstrap/js/bootstrap.bundle.min.js'
     },
     pluginJs: './app/plugins/**/*.js',
 
-    plugin :{
+    plugin: {
         js: './app/plugin/js/*.js',
         css: './app/plugin/css/*.css'
     }
@@ -59,20 +74,21 @@ const path = {
 
 
 const dest = {
-    css : destination + 'css/',
-    scss : destination + 'scss/',
-    js : destination + 'js/',
-    fonts : destination + 'fonts/',
-    img : destination + 'image/',
-    plugins : destination + 'plugins/',
-    temp : destination + 'temp/',
+    css: destination + 'css/',
+    scss: destination + 'scss/',
+    js: destination + 'js/',
+    fonts: destination + 'fonts/',
+    img: destination + 'image/',
+    plugins: destination + 'plugins/',
+    temp: destination + 'temp/',
+    mainPageDist: destination + 'page/',
     bundle: {
-        css:'bundled/css/',
+        css: 'bundled/css/',
         js: 'bundled/js/'
     }
 }
 
-const watchSrc = [path.html, path.js, path.php, path.img, path.fonts, path.plugin.css,path.plugin.css,path.plugin];
+const watchSrc = [path.html, path.js, path.php, path.img, path.fonts, path.plugin.css, path.plugin.css, path.plugin];
 
 
 
@@ -81,14 +97,14 @@ const watchSrc = [path.html, path.js, path.php, path.img, path.fonts, path.plugi
 ===================================================== */
 function browserReload(done) {
     browserSync.init({
-      server: {
-        baseDir: destination + '/'
-      },
-      port: port
+        server: {
+            baseDir: destination + '/'
+        },
+        port: port
     });
     done();
-  }
-  
+}
+
 
 
 
@@ -117,44 +133,48 @@ function customPlumber([errTitle]) {
 /* =====================================================
     HTML
 ===================================================== */
-    function html () {
-        return gulp.src([path.html])
+function html() {
+    buildDynamicPage();
+    return gulp.src([path.html])
         .pipe(rendeNun({
-                    path: [path._partial] // String or Array
-                }))
+            path: [path._partial] // String or Array
+        }))
         .pipe(customPlumber('Error Running Nunjucks'))
         .pipe(beautify({
-                    indent_size: 2,
-                    indent_char: ' ',
-                    max_preserve_newlines: 0,
-                    unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br']
-                }))
+            indent_size: 2,
+            indent_char: ' ',
+            max_preserve_newlines: 0,
+            unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br']
+        }))
         .pipe(gulp.dest(destination))
         .pipe(browserSync.reload({
             stream: true
         }));
-    };
+
+};
 
 /* =====================================================
     css
 ===================================================== */
-    function scss() {
-        return gulp.src([path.scss])
-            .pipe(gulpif(sourcemap, sourcemaps.init()))
-            .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-            .pipe(autoPrefixer())
-            .pipe(gulpif(argv.demo, csso({
-                restructure: false,
-                sourceMap: true,
-                debug: true
-            })))
-            .pipe(gulpif(sourcemap, sourcemaps.write('./maps/')))
-            .pipe(lineec())
-            .pipe(gulp.dest(dest.css))
-            .pipe(browserSync.reload({
-                stream: true
-            }));
-    };
+function scss() {
+    return gulp.src([path.scss])
+        .pipe(gulpif(sourcemap, sourcemaps.init()))
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }).on('error', sass.logError))
+        .pipe(autoPrefixer())
+        .pipe(gulpif(argv.demo, csso({
+            restructure: false,
+            sourceMap: true,
+            debug: true
+        })))
+        .pipe(gulpif(sourcemap, sourcemaps.write('./maps/')))
+        .pipe(lineec())
+        .pipe(gulp.dest(dest.css))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+};
 
 
 /* =====================================================
@@ -167,39 +187,47 @@ function cleanTemp() {
 function concatPluginCss() {
     return gulp.src([path.pluginCss, "!./app/plugins/bootstrap/css/bootstrap.min.css"])
         .pipe(concat('plugins.css'))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest(path.temp))
 }
+
 function concatCssFinal() {
-    return gulp.src([path.bootstrap.css, path.temp + "plugins.css"],{ allowEmpty: true })
-            .pipe(concat('plugins.min.css'))
-            .pipe(lineec())
-            .pipe(gulp.dest(dest.bundle.css))
+    return gulp.src([path.bootstrap.css, path.temp + "plugins.css"], {
+            allowEmpty: true
+        })
+        .pipe(concat('plugins.min.css'))
+        .pipe(lineec())
+        .pipe(gulp.dest(dest.bundle.css))
 }
 
 function concatPluginJs() {
-    
-    return gulp.src([path.pluginJs, "!./app/plugins/bootstrap/js/bootstrap.bundle.min.js",'!app/plugins/jquery/jquery.min.js',"!app/plugins/jquery/jquery-migrate.min.js"])
-        .pipe(concat('plugins.min.js' ))
+
+    return gulp.src([path.pluginJs, "!./app/plugins/bootstrap/js/bootstrap.bundle.min.js", '!app/plugins/jquery/jquery.min.js', "!app/plugins/jquery/jquery-migrate.min.js"])
+        .pipe(concat('plugins.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(path.temp))
 }
+
 function concatJsFinal() {
     return gulp.src([
-        "./app/plugins/jquery/jquery.min.js",
-        "./app/plugins/jquery/jquery-migrate.min.js",
-        path.bootstrap.js, 
-        path.temp + "plugins.min.js",
-    ],{ allowEmpty: true })
-            .pipe(concat('plugins.min.js'))
-            .pipe(uglify())
-            .pipe(lineec())
-            .pipe(gulp.dest(dest.bundle.js))
+            "./app/plugins/jquery/jquery.min.js",
+            "./app/plugins/jquery/jquery-migrate.min.js",
+            path.bootstrap.js,
+            path.temp + "plugins.min.js",
+        ], {
+            allowEmpty: true
+        })
+        .pipe(concat('plugins.min.js'))
+        .pipe(uglify())
+        .pipe(lineec())
+        .pipe(gulp.dest(dest.bundle.js))
 }
 
-const pluginCss = gulp.series(concatPluginCss,concatCssFinal);
-const pluginJs = gulp.series(concatPluginJs,concatJsFinal);
-const pluginminify = gulp.series(gulp.parallel(pluginCss,pluginJs), cleanTemp);
+const pluginCss = gulp.series(concatPluginCss, concatCssFinal);
+const pluginJs = gulp.series(concatPluginJs, concatJsFinal);
+const pluginminify = gulp.series(gulp.parallel(pluginCss, pluginJs), cleanTemp);
 
 /* =====================================================
     Copy SCSS Folder
@@ -212,13 +240,19 @@ function sassCopy() {
 /* =====================================================
     Image
 ===================================================== */
-function imgmin(){
+function imgmin() {
     return gulp.src([path.img])
         .pipe(changed(dest.img))
         .pipe(gulpif(minImg, imagemin([
-            imagemin.gifsicle({ interlaced: true }),
-            imagemin.jpegtran({ progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 })
+            imagemin.gifsicle({
+                interlaced: true
+            }),
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            imagemin.optipng({
+                optimizationLevel: 5
+            })
         ])))
         .pipe(gulp.dest(dest.img))
         .pipe(browserSync.reload({
@@ -241,7 +275,7 @@ function javascript() {
 ===================================================== */
 function fonts() {
     return gulp.src([path.fonts])
-    .pipe(changed(dest.fonts))
+        .pipe(changed(dest.fonts))
         .pipe(gulp.dest(dest.fonts));
 }
 
@@ -251,7 +285,7 @@ function fonts() {
 ===================================================== */
 function plugins() {
     return gulp.src([path.plugins])
-    .pipe(changed(dest.plugins))
+        .pipe(changed(dest.plugins))
         .pipe(gulp.dest(dest.plugins));
 }
 
@@ -273,42 +307,85 @@ function sassCopy() {
         .pipe(gulpif(argv.pub, gulp.dest(dest.scss)))
 };
 
-
+// function for copy template file
+function copyPageAndRename() {
+    return gulp
+        .src("./app/page-template.njk")
+        .pipe(rendeNun({
+            path: [path._partial]
+        }))
+        .pipe(rename("./Page/Pagex.html"))
+        .pipe(gulp.dest("./dist"));
+}
 
 function watchFiles() {
     gulp.watch(path.html, html);
     gulp.watch(path._partial, html);
-    gulp.watch([path.plugin.js,path.js,path.fonts,path.img], copyAssets);
+    gulp.watch([path.plugin.js, path.js, path.fonts, path.img], copyAssets);
     gulp.watch(path.scss, scss);
-    gulp.watch(path.root, gulp.series(clean,build));
-  }
+    gulp.watch(path.root, gulp.series(clean, build));
+}
 
 
 
-const copyAssets = gulp.parallel(fonts, javascript, sassCopy, plugins,imgmin);
-const build = gulp.series(clean, html, gulp.parallel(scss, copyAssets));
-const buildWatch = gulp.series(build,browserReload, gulp.parallel(watchFiles));
+function buildDynamicPage() {
+    var nunjucksIncludeString = convertInputPageLayoutThemeObjectToNujucksIncludeString(inputPageLayoutTheme);
+    // makeCopyPageTemplateToPageName(pageName);
+    stringReplace(pageName, "@@sectionContent@@", nunjucksIncludeString);
+}
+
+function convertInputPageLayoutThemeObjectToNujucksIncludeString(inputPageLayoutTheme) {
+    let pageNameChoice = "";
+    for (let i = 0; i < inputPageLayoutTheme.sectionDetails.length; i++) {
+        pageNameChoice += '{% include "_'+ inputPageLayoutTheme.sectionDetails[i].sectionName + inputPageLayoutTheme.sectionDetails[i].sectionChoiceNumber + inputPageLayoutTheme.theme + '.njk"%}' + "\n";
+    }
+    return pageNameChoice;
+}
+
+function makeCopyPageTemplateToPageName() {
+    console.log(pageName);
+    return gulp
+        .src("./app/page-template.njk.temp")
+        .pipe(rename( pageName + ".njk"))
+        .pipe(gulp.dest("./app"));
+
+}
+
+function stringReplace(pageName, findStr, replaceStr) {
+    return gulp.src("./app/" + pageName + ".njk")
+        .pipe(replace(new RegExp(findStr, 'g'), replaceStr))
+        .pipe(gulp.dest("./app"));
+
+}
+
+function makeCopyPageTemplateToPageNameTest() {
+    return makeCopyPageTemplateToPageName("raju");
+}
+
+function stringReplaceTest() {
+    return stringReplace("raju", "@@sectionContent@@", "@@hero@@");
+}
+
+
+const copyAssets = gulp.parallel(fonts, javascript, sassCopy, plugins, imgmin);
+const build = gulp.series(clean, makeCopyPageTemplateToPageName, html, gulp.parallel(scss, copyAssets));
+const buildWatch = gulp.series(build, browserReload, gulp.parallel(watchFiles));
 
 
 
 
 exports.html = html;
 exports.imgmin = imgmin;
-exports. browserReload =  browserReload;
+exports.browserReload = browserReload;
 exports.pluginJs = pluginJs;
 exports.pluginCss = pluginCss;
 exports.scss = scss;
 exports.clean = clean;
 exports.build = build;
 exports.buildWatch = buildWatch;
-exports.watchFiles =  watchFiles;
+exports.watchFiles = watchFiles;
 exports.watchSrc = watchSrc;
 exports.default = buildWatch;
 exports.copyAssets = copyAssets;
 exports.pluginminify = pluginminify;
-
-
-
-
-
-
+exports.buildDynamicPage = buildDynamicPage;
